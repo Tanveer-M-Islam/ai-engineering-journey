b8449f151a8807d92585fdbc647bfbce643f91ac
tanveer_m_islam <tanvirbest7@gmail.com>
2026-05-14 21:01:27 +0600

    Project 2,Build Tools

---
+++ projects/02-student-assistant-agent/tools.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import time
+
+
+def get_time():
+
+    return datetime.now().strftime(
+        "%I:%M %p"
+    )
+
+
+def get_date():
+
+    return datetime.now().strftime(
+        "%d-%m-%Y"
+    )
+
+
+def multiply(a, b):
+
+    return a * b
+
+
+def calculate_gpa(
+    total_gp,
+    total_courses
+):
+
+    return round(
+        total_gp / total_courses,
+        2
+    )
+
+
+def start_timer(seconds):
+
+    time.sleep(seconds)
+
+    return "Study session completed."
