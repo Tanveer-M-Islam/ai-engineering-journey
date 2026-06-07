0a5791e4260968c217fbb312dfc1eb09a74c6950
tanveer_m_islam <tanvirbest7@gmail.com>
2026-06-07 19:23:33 +0600

    Build LangChain RAG system

---
+++ practice/Day25_langchain_rag/build_index.py
@@ -0,0 +1,34 @@
+from langchain_community.vectorstores import FAISS
+
+from langchain_community.embeddings import (
+    HuggingFaceEmbeddings
+)
+
+with open(
+    "knowledge.txt",
+    "r",
+    encoding="utf-8"
+) as f:
+
+    text = f.read()
+
+chunks = text.split("\n\n")
+
+embedding_model = (
+    HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+)
+
+vectorstore = FAISS.from_texts(
+    chunks,
+    embedding_model
+)
+
+vectorstore.save_local(
+    "faiss_index"
+)
+
+print(
+    "Vector database created."
+)
